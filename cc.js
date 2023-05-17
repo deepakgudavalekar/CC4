cb1f6dfd2191ff582ee7a2b57a766cdc344d29bd
deepakgudavalekar <133839257+deepakgudavalekar@users.noreply.github.com>
2023-05-17 12:01:59 +0530

    Add files via upload

---
+++ cc.js
@@ -0,0 +1,7 @@
+
+    function Add()
+    {
+        a=parseInt(document.getElementById('num1').value);
+        b=parseInt(document.getElementById('num2').value);
+        document.getElementById('result').innerHTML=a+b;
+    }
